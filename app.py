6180c1eff6734d36bfde2737fea22a2a6c5b4e67
Gaurav Sahani <63406916+GauravSahani1417@users.noreply.github.com>
2020-07-04 16:28:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,86 @@
+from __future__ import division, print_function
+# coding=utf-8
+import os
+import numpy as np
+
+# Keras
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import tensorflow
+
+# Flask utils
+from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
+#from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+model = tensorflow.keras.models.load_model('model_Classifier.h5')
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(224, 224))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    # x = np.true_divide(x, 255)
+    ## Scaling
+    x = np.expand_dims(x, axis = 0)
+
+    preds = model.predict(x)
+    preds=np.argmax(preds, axis=1)
+    if preds==0:
+        preds="This is One Hundred Rupees Note!"
+    elif preds==1:
+        preds="This is Two Hundred Rupees Note!"
+    elif preds==2:
+        preds="This is Two Thousand Rupees Note!"
+    elif preds==3:
+        preds="This is Five Hundred Rupees Note!"
+    elif preds==4:
+        preds="This is Fifety Rupees Note!"
+    elif preds==5:
+        preds="This is Ten Rupees Note!"    
+    else:
+        preds="This is Twenty Rupees Note"
+    
+    
+    return preds
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+        result=preds
+        return result
+    return None
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
